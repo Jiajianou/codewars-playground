89f1beabcf481a0648e2f9947cc9ab511fa57655
J <Jiajian.ou@gmail.com>
2019-12-30 08:08:13 -0500

    daily dec 30.

---
+++ removeVowels.js
@@ -0,0 +1,28 @@
+// This Kata is intended as a small challenge for my students
+
+// Create a function, called removeVowels (or remove_vowels), that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+// removeVowels("drake") // => "drk"
+// removeVowels("aeiou") // => ""
+
+
+
+
+
+const removeVowels = s => s.replace(/[a|e|i|o|u]/ig, "");
+
+
+
+
+console.log(removeVowels("drake"));
+
+
+
+//cw's 
+
+function removeVowels2(str){
+    return str.replace(/[aeiou]/ig,'')
+  }
+
+
+
