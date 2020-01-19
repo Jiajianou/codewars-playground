406a5ba9a0580621197c81c20e858e0f6f618a53
J <Jiajian.ou@gmail.com>
2020-01-19 08:22:51 -0500

    daily jan 19

---
+++ orderPlease.js
@@ -0,0 +1,45 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+
+
+const order = s => {
+
+    if(s.length === 0 ) return "";
+
+    let output = [];
+
+    s.split(" ").forEach(word => {
+
+        let index = word.match(/[0-9]/)[0]-1;
+
+        output[index] = word;
+
+    });
+
+    return output.join(" ");
+
+}
+
+console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
+
+
+function order2(words){
+  
+    return words.split(' ').sort(function(a, b){
+        return a.match(/\d/) - b.match(/\d/);
+     }).join(' ');
+  }  
+
+
+  function order3(words) {
+    return words.split(' ').sort((wordA, wordB) => wordA.match(/\d+/) > wordB.match(/\d+/)).join(' ')
+  }
